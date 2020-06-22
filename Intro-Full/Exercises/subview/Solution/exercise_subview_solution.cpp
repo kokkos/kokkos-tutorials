3151b13c368f4a09fabc481126fdd2be9fabb971
Daniel Arndt <arndtd@ornl.gov>
2020-06-22 19:00:46 +0000

    Allow using HIP as execution space

--- Intro-Full/Exercises/subview/Solution/exercise_subview_solution.cpp
+++ Intro-Full/Exercises/subview/Solution/exercise_subview_solution.cpp
@@ -96,11 +96,13 @@ int main( int argc, char* argv[] )
   // typedef Kokkos::Threads  ExecSpace;
   typedef Kokkos::OpenMP   ExecSpace;
   // typedef Kokkos::Cuda     ExecSpace;
+  // typedef Kokkos::Experimental::HIP ExecSpace;
 
   // typedef Kokkos::HostSpace     MemSpace;
   typedef Kokkos::OpenMP        MemSpace;
   // typedef Kokkos::CudaSpace     MemSpace;
   // typedef Kokkos::CudaUVMSpace  MemSpace;
+  // typedef Kokkos::Experimental::HIPSpace MemSpace;
 
   // typedef Kokkos::LayoutLeft   Layout;
   typedef Kokkos::LayoutRight  Layout;

b6d115b45522cc6ea24b1b00b9cd44a54c2ecacc
jeffmiles63 <jsmiles@apollo.sandia.gov>
2020-08-04 16:05:41 -0600

    update solution to work with cuda and openmp

--- Exercises/unique_token/Solution/unique_token.cpp
+++ Exercises/unique_token/Solution/unique_token.cpp
@@ -1,11 +1,17 @@
 #include<Kokkos_Core.hpp>
 
+using atomic_1d_view = Kokkos::View<int*, Kokkos::DefaultExecutionSpace, 
+                                           Kokkos::MemoryTraits<Kokkos::Atomic> >;
+using atomic_2d_view = Kokkos::View<int**, Kokkos::DefaultExecutionSpace, 
+                                           Kokkos::MemoryTraits<Kokkos::Atomic> >;
 // Scatter Add algorithm using data replication
 double scatter_add_loop(Kokkos::View<int**> v, 
-		 Kokkos::View<int*> r) {
+		 atomic_1d_view r) {
   Kokkos::Timer timer;
-  Kokkos::Experimental::UniqueToken<Kokkos::DefaultExecutionSpace> tokens(2048);
-  Kokkos::View<int**> results("Rdup",tokens.size(),r.extent(0));
+  Kokkos::Experimental::UniqueToken<Kokkos::DefaultExecutionSpace> tokens;
+  int dup_size = tokens.size() > 2048 ? 2048 : tokens.size();
+  int dup_mask = 2048 - 1;
+  atomic_2d_view results("Rdup",dup_size,r.extent(0));
 
   // Reset duplicated array
   Kokkos::deep_copy(results,0);
@@ -13,20 +19,24 @@ double scatter_add_loop(Kokkos::View<int**> v,
   Kokkos::parallel_for("Duplicated Loop", 
     Kokkos::RangePolicy<>(0,v.extent(0)),
     KOKKOS_LAMBDA(const int i) {
+
     // Every thread contribues to its version of the vector
     Kokkos::Experimental::AcquireUniqueToken<Kokkos::DefaultExecutionSpace> token_val(
         tokens);
-    const int32_t tid = token_val.value();
+    const int32_t tid = token_val.value() & dup_mask;
     for(int j=0; j<v.extent(1); j++)
       results(tid,v(i,j))++;
   });
 
   // Contribute back to the single version
   Kokkos::parallel_for("Reduce Loop",
-    Kokkos::RangePolicy<>(0,v.extent(0)),
-    KOKKOS_LAMBDA(const int i) {
-    for(int tid=0; tid<results.extent(0); tid++)
-      r(i)+=results(tid,i);
+    Kokkos::TeamPolicy<>(v.extent(0), Kokkos::AUTO),
+    KOKKOS_LAMBDA(Kokkos::TeamPolicy<>::member_type team) {
+       int i = team.league_rank();
+       Kokkos::parallel_for( Kokkos::TeamVectorRange(team, 
+                                results.extent(0)), [=] (int tid) {
+          r(i)+=results(tid,i);
+       });
   });
 
   Kokkos::fence();

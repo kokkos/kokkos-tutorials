adc51f16307161d4dcdb830d308d35cb08bc9654
Damien L-G <dalg24@gmail.com>
2023-06-02 18:38:53 -0400

    Improve solution
    Co-Authored-By: Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
    Co-Authored-By: Daniel Arndt <arndtd@ornl.gov>

--- Exercises/common.cmake
+++ Exercises/common.cmake
@@ -8,21 +8,12 @@ endif()
 
 set(Kokkos_COMMON_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../../dep/Kokkos)
 
-include(FetchContent)
-if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24)
-  FetchContent_Declare(
-    Kokkos
-    GIT_REPOSITORY https://github.com/kokkos/kokkos.git
-    GIT_TAG        4.0.01
-    SOURCE_DIR ${Kokkos_COMMON_SOURCE_DIR}
-    FIND_PACKAGE_ARGS
-  )
-  FetchContent_MakeAvailable(Kokkos)
-  
-  find_package(Kokkos REQUIRED)
-else()
-  find_package(Kokkos)
-  if(NOT Kokkos_FOUND)
+find_package(Kokkos CONFIG)
+if(NOT Kokkos_FOUND)
+  if(EXISTS ${Kokkos_COMMON_SOURCE_DIR})
+    add_subdirectory(${Kokkos_COMMON_SOURCE_DIR} Kokkos)
+  else()
+    include(FetchContent)
     FetchContent_Declare(
       Kokkos
       GIT_REPOSITORY https://github.com/kokkos/kokkos.git

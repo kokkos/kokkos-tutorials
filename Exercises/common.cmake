95f9b6ab406a6c6e847ae6382a557e993aebf441
Damien L-G <dalg24+github@gmail.com>
2026-03-11 08:38:03 -0600

    Update Kokkos GIT_TAG to version 5.0.0

--- Exercises/common.cmake
+++ Exercises/common.cmake
@@ -19,7 +19,7 @@ else()
     FetchContent_Declare(
       Kokkos
       GIT_REPOSITORY https://github.com/kokkos/kokkos.git
-      GIT_TAG        4.7.00
+      GIT_TAG        5.0.0
       SOURCE_DIR ${Kokkos_COMMON_SOURCE_DIR}
     )
     FetchContent_MakeAvailable(Kokkos)
